4ed0ff7fd942378ed6c553a67e539175fb7f7ab8
MariamSarag0000 <62335913+MariamSarag0000@users.noreply.github.com>
2020-12-19 01:17:20 +0200

    Update UpdatePasswordRequest.php

--- app/Http/Requests/UpdatePasswordRequest.php
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -24,7 +24,8 @@ class UpdatePasswordRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'email'=>'required|email',
+            'password'=>'required|confirmed'
         ];
     }
 }
