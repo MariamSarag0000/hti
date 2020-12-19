0efae3a46ddba5f219b82cb5deaa99a04c2338f6
MariamSarag0000 <62335913+MariamSarag0000@users.noreply.github.com>
2020-12-19 21:24:05 +0200

    Update ForgotPasswordController.php

--- app/Http/Controllers/Api/Student/ForgotPasswordController.php
+++ app/Http/Controllers/Api/Student/ForgotPasswordController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\Api\Student;
 
+use App\ApiCode;
+use App\Http\Requests\UpdatePasswordRequest;
+use Illuminate\Support\Facades\Password;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -15,64 +19,6 @@ use Illuminate\Support\Str;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-
-    public function sendEmail(Request $request){
-        if(!$this->validateEmail($request->email)){
-            return $this->failedResponse();
-        }
-        $this->send($request->email);
-        return $this->successResponse();
-    }
-
-    public function send($email){
-        $token=$this->createToken($email);  //token is important to send code
-        Mail::to($email)->send(new SendMailReset($token,$email));
-    }
-
-    public function createToken($email)  // this is a function to get your request email that there are or not to send mail
-    {
-        $oldToken = DB::table('password_resets')->where('email', $email)->first();
-
-        if ($oldToken) {
-            return $oldToken->token;
-        }
-
-        $token = Str::random(40);
-        $this->saveToken($token, $email);
-        return $token;
-    }
-
-
-    public function saveToken($token, $email)  // this function save new password
-    {
-        DB::table('password_resets')->insert([
-            'email' => $email,
-            'token' => $token,
-            'created_at' => Carbon::now()
-        ]);
-    }
-
-    public function validateEmail($email)  //this is a function to get your email from database
-    {
-        return !!Student::where('email', $email)->first();
-    }
-
-    public function failedResponse()
-    {
-        return response()->json([
-            'error' => 'Email does\'t found on our database'
-        ], Response::HTTP_NOT_FOUND);
-    }
-
-    public function successResponse()
-    {
-        return response()->json([
-            'data' => 'Reset Email is send successfully, please check your inbox.'
-        ], Response::HTTP_OK);
-    } */
-
-
 
     // The new one:
     public function forgot(){
@@ -81,24 +27,19 @@ class ForgotPasswordController extends Controller
         return response()->json('Reset password link sent to your email');
     }
 
-    public function reset(){
-        request()->validate()([
-            'email'=>'required|email',
-            'password'=>'required|sring|max:25|confirmed',
-            'token'=>'required|string'
-        ]);
+    public function reset(ResetPasswordRequest $request){
+        $credentials=request()->validate();
 
-        $email_password_status=Password::reset($credentials, function ($user,$password){    //it is the new password 
+        $email_password_status=Password::reset($request->validate(),$credentials, function ($user,$password){    //it is the new password 
             $user->password=$password;
             $user->save();
         });
 
+        if($email_password_status==Password::INVALID_TOKEN){
+            return $this->responsBadRequest(ApiCode::INVALID_RESET_PASSWORD_TOKEN);
+        }
 
-
-
+        return $this->respondWithMessage("Password has been successfully changed");
     }
-   
-
-
 
 }
