f82e9678745e2c06823d1b370bd739c85f8b5c02
MariamSarag0000 <62335913+MariamSarag0000@users.noreply.github.com>
2020-12-23 18:32:57 +0200

    Update Stuff.php

--- app/Stuff.php
+++ app/Stuff.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stuff extends Model
 {
-    //
+    protected $table='stuffs';
+    
+    protected $fillable = [
+            'name_en',
+            'name_ar',
+            'email',
+            'password',
+            'stuff_id',
+            'address',
+            'phone',
+            'birthdate',
+            'salary',
+            'status',
+    ];
+
+    
 }
